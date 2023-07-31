737ee5e088bd327d7f2ad2c851dd90a57dba4f9d
BuildWithData <@Buildwd>
2023-07-31 20:23:39 +0200

    analytics

--- pytube/code/analytics.py
+++ pytube/code/analytics.py
@@ -1,5 +1,4 @@
 from datetime import date
-from math import ceil
 import matplotlib.pyplot as plt
 from matplotlib.pyplot import Figure
 import pandas as pd
@@ -13,8 +12,8 @@ class Analytics:
 
     def _heatmap(self, df: DataFrame) -> DataFrame:
         return (
-            df 
-            .style.background_gradient(cmap ='YlOrRd', axis=None)
+            df
+            .style.background_gradient(cmap='YlOrRd', axis=None)
             .set_properties(**{'font-size': '20px'})
         )
 
@@ -22,7 +21,7 @@ class Analytics:
         df = pd.read_csv(f"./data/{self.name}.csv")
         df["publish_date"] = df.publish_date.apply(lambda s: date.fromisoformat(s))
         df["year"] = df.publish_date.apply(lambda d: d.year)
-        df["month"] = df.publish_date.apply(lambda d: d.month) 
+        df["month"] = df.publish_date.apply(lambda d: d.month)
         df["minutes"] = df.length / 60
 
         self.data = df
@@ -40,12 +39,11 @@ class Analytics:
 
         for c in videos_per_month.columns:
             videos_per_month = videos_per_month.astype({c: int})
-            
+
         videos_per_month["tot"] = videos_per_month.apply(lambda r: sum(r), axis=1)
         videos_per_month["mean"] = videos_per_month.tot.apply(lambda n: int(round(n / 12)))
 
         if heatmap is True:
-
             videos_per_month = self._heatmap(videos_per_month[list(range(1, 13))])
 
         return videos_per_month
@@ -59,25 +57,35 @@ class Analytics:
         plt.bar(videos_per_year.year, videos_per_year.tot)
 
         for x, y in zip(videos_per_year.year, videos_per_year.tot):
-            plt.text(x, y, y, ha = 'center')
-            
-        plt.tick_params(left=False, labelleft = False)
+            plt.text(x, y, y, ha='center')
+
+        plt.tick_params(left=False, labelleft=False)
         plt.title("#VIDEOS")
 
         plt.close()
 
-        return fig 
-        
+        return fig
+
     def get_views_from_month(
-        self, 
-        weight: int = 6, 
+        self,
+        weight: int = 6,
+        from_: str = None,
+        to: str = None,
         videos_info: bool = False,
         heatmap: bool = False
     ) -> DataFrame:
 
+        data = self.data
+
+        if from_ is not None:
+            data = data[data.publish_date >= date.fromisoformat(from_)]
+
+        if to is not None:
+            data = data[data.publish_date <= date.fromisoformat(to)]
+
         views_from_month = (
 
-            self.data
+            data
             .groupby(["year", "month"]).sum("views")
             .reset_index()
             .pivot(values="views", columns="month", index="year")
@@ -86,38 +94,52 @@ class Analytics:
 
         views_from_month["tot"] = views_from_month.apply(lambda r: sum(r), axis=1)
         views_from_month["mean"] = round(views_from_month.tot / 12)
-            
+
         views_from_month = views_from_month / (10 ** weight)
 
         for c in views_from_month.columns:
             views_from_month = views_from_month.astype({c: int})
 
         if videos_info is True:
-            
+
             videos_per_month = self.get_videos_per_month()
 
             views_from_month["n_videos"] = videos_per_month["tot"]
             views_from_month["per_video"] = views_from_month.apply(lambda r: int(r.tot / r.n_videos), axis=1)
-    
+
         if heatmap is True:
             views_from_month = self._heatmap(views_from_month[list(range(1, 13))])
-    
-        return views_from_month 
 
-    def get_avg_views_from_month(self, weight: int = 6, heatmap: bool = False) -> DataFrame:
+        return views_from_month
+
+    def get_avg_views_from_month(
+        self,
+        weight: int = 6,
+        from_: str = None,
+        to: str = None,
+        heatmap: bool = False
+    ) -> DataFrame:
+
+        data = self.data
+
+        if from_ is not None:
+            data = data[data.publish_date >= date.fromisoformat(from_)]
+
+        if to is not None:
+            data = data[data.publish_date <= date.fromisoformat(to)]
 
         avg_views_from_month = (
-            self.data 
+            data
             .groupby(["year", "month"]).size()
             .reset_index()
             .rename({0: "tot"}, axis=1)
 
             .merge(
-                
-                self.data 
+
+                self.data
                 .groupby(["year", "month"]).sum("views")
                 .reset_index(),
-            
+
                 on=["year", "month"]
             )
         )
@@ -128,48 +150,72 @@ class Analytics:
 
         for c in avg_views_from_month.columns:
             avg_views_from_month = avg_views_from_month.astype({c: int})
-            
+
         if heatmap is True:
-            avg_views_from_month = self._heatmap(avg_views_from_month) 
+            avg_views_from_month = self._heatmap(avg_views_from_month)
 
         return avg_views_from_month
 
-    def get_total_minutes_per_month(self, heatmap: bool = True) -> DataFrame:
+    def get_total_minutes_per_month(
+        self,
+        from_: str = None,
+        to: str = None,
+        heatmap: bool = True
+    ) -> DataFrame:
+
+        data = self.data
+
+        if from_ is not None:
+            data = data[data.publish_date >= date.fromisoformat(from_)]
+
+        if to is not None:
+            data = data[data.publish_date <= date.fromisoformat(to)]
 
         minutes_per_month = (
-    
-            self.data 
+            data
             .groupby(["year", "month"]).sum("minutes")
             .reset_index()
             .pivot(values="minutes", columns="month", index="year")
             .fillna(0)
-            
         )
 
         for c in minutes_per_month.columns:
             minutes_per_month = minutes_per_month.astype({c: int})
-            
+
         if heatmap is True:
             minutes_per_month = self._heatmap(minutes_per_month)
 
         return minutes_per_month
 
-    def get_avg_minutes_per_month(self, heatmap: bool = False) -> DataFrame:
-   
+    def get_avg_minutes_per_month(
+        self,
+        from_: str = None,
+        to: str = None,
+        heatmap: bool = False
+    ) -> DataFrame:
+
+        data = self.data
+
+        if from_ is not None:
+            data = data[data.publish_date >= date.fromisoformat(from_)]
+
+        if to is not None:
+            data = data[data.publish_date <= date.fromisoformat(to)]
+
         minutes_per_month = (
 
-            self.data 
+            data
             .groupby(["year", "month"]).size()
             .reset_index()
             .rename({0: "tot"}, axis=1)
-            
+
             .merge(
 
-                self.data 
+                self.data
                 .groupby(["year", "month"]).sum("minutes")
                 .reset_index(),
                 on=["year", "month"]
-            
+
             )
         )
 
@@ -178,13 +224,16 @@ class Analytics:
         minutes_per_month = minutes_per_month.pivot(values="mean", columns="month", index="year").fillna(0)
 
         for c in minutes_per_month.columns:
-            
+
             minutes_per_month = minutes_per_month.astype({c: int})
-            
+
         minutes_per_month["tot"] = minutes_per_month.apply(lambda r: sum(r), axis=1)
         minutes_per_month["mean"] = minutes_per_month.tot.apply(lambda n: int(round(n / 12)))
 
         if heatmap is True:
-            minutes_per_month = self._heatmap(minutes_per_month)
+            minutes_per_month = self._heatmap(minutes_per_month[list(range(1, 13))])
 
         return minutes_per_month
+
+    def get_top_videos_by_views(self, top: int) -> DataFrame:
+        return self.data.sort_values("views", ascending=False).reset_index(drop=True).head(top)
